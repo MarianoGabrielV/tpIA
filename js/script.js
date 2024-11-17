a6238f88d826ee4ba037c252a11d2538c1fbf22c
MarianoVergniaud <mariano.unla@gmail.com>
2024-11-17 17:20:13 -0300

    soloFaltanStilos

--- js/script.js
+++ js/script.js
@@ -7,3 +7,20 @@ document.querySelectorAll('a.nav-link').forEach(anchor => {
         });
     });
 });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const text = "Clasificación No Supervisada";
+    const typingElement = document.getElementById("typing-effect");
+    let index = 0;
+
+    function typeLetter() {
+        if (index < text.length) {
+            typingElement.innerHTML += text.charAt(index);
+            index++;
+            setTimeout(typeLetter, 100); // Velocidad de escritura (100 ms por letra)
+        }
+    }
+
+    typeLetter();
+});
